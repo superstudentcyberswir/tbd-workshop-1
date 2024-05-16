604d6de5433dc050bba0544edccdbea2b0f869e2
superstudentcyberswir <01195923@pw.edu.pl>
2024-05-16 17:54:15 +0200

    installation

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -2,10 +2,12 @@ provider "google" {
   project = local.project
   region  = var.region
 }
+
 terraform {
-  required_version = "~> 1.5.0"
+  required_version = "~> 1.8.3"
   required_providers {
     google = {
+      source  = "hashicorp/google"
       version = "~> 5.23.0"
     }
   }
--- cicd_bootstrap/provider.tf
+++ cicd_bootstrap/provider.tf
@@ -3,9 +3,10 @@ provider "google" {
   region  = var.region
 }
 terraform {
-  required_version = "~> 1.5.0"
+  required_version = "~> 1.8.3"
   required_providers {
     google = {
+      source  = "hashicorp/google"
       version = "~> 5.23.0"
     }
   }
